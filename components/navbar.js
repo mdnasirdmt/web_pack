1c51a1e7ca6ab18c6d6ab2ec7b4dd48b09be6a74
Md Nasir Uddin <mdnasirdmt@gmail.com>
2022-04-29 14:16:51 +0530

    web pack taking too much time

---
+++ components/navbar.js
@@ -0,0 +1,25 @@
+function navbar()
+{
+    return `
+    <div id="navbar">
+    <h3>
+        <a href="./index.html">Home</a>
+    </h3>
+    <div id="options">
+        <h3>
+            <a href="jewelery.html">Jewelery</a>
+        </h3>
+        <h3>
+            <a href="electronics.html">Electronics</a>
+        </h3>
+        <h3>
+            <a href="login.html">Login</a>
+        </h3>
+        <h3>
+            <a href="signup.html">SignUp</a>
+        </h3>
+    </div>
+</div>
+    `
+}
+export {navbar};
---
+++ src/index.js
@@ -0,0 +1,6 @@
+
+import {navbar} from '../components/navbar';
+// console.log(navbar);
+import '../styles/navbar.css'
+let navbar_div=document.getElementById('navba')
+navbar_div.innerHTML=navbar();
